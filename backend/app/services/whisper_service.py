b8c72186a023a187f737b9eb2f3953ff5a420915
Riyaz <riyazmalik2046@gmail.com>
2026-06-17 13:18:51 +0530

    migrate whisper to groq

--- backend/app/services/whisper_service.py
+++ backend/app/services/whisper_service.py
@@ -1,9 +1,19 @@
-import whisper
+from groq import Groq  # type: ignore[import]
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+client = Groq(
+    api_key=os.getenv("GROQ_API_KEY")
+)
 
 def transcribe_audio(file_path: str):
 
-    model = whisper.load_model("base")
+    with open(file_path, "rb") as audio_file:
+        transcription = client.audio.transcriptions.create(
+            file=audio_file,
+            model="whisper-large-v3"
+        )
 
-    result = model.transcribe(file_path)
-
-    return result["text"]
+    return transcription.text
